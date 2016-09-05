f0115edd13319f940a26967b41743d437aea2556
chenchen <chenchencswpi@gmail.com>
2016-09-05 13:54:14 -0400

    add plus one problem

---
+++ LeetCode/src/array/PlusOne.java
@@ -0,0 +1,16 @@
+package array;
+
+/**
+ * Created by chenchen on 9/4/16.
+ */
+public class PlusOne {
+    public static void main(String[] args) {}
+
+}
+
+class PlusOneSolution {
+    public int[] plusOne(int[] digits) {
+
+    }
+}
+
