28537aea62c9c9e2660bcd3a976b2a84b70211d6
Chen Chen <Chen.Chen@tradingscreen.com>
2016-09-07 16:27:13 -0400

    revise

--- LeetCode/src/math/ReverseInteger.java
+++ LeetCode/src/math/ReverseInteger.java
@@ -1,25 +1,27 @@
 package math;
 
+/**
+ * <a href="https://leetcode.com/problems/reverse-integer/">原题链接</a>
+ * Reverse digits of an integer. For example, 123 -> 321, -123 -> -321.
+ */
+
+/**
+ * 题目的意思是反转一个数字。做这种数字反转题大家一定要留意越界，越界，越界！重要的事情说三遍。
+ * 如果存在越界可能，问面试官他期待的是什么。在这道题里，题目规定越界返回0就可以了。
+ * @author cchen
+ *
+ */
 public class ReverseInteger {
-
-}
-
-class ReverseIntegerSolution {
-	/**
-	 * Reverse an integer. For example, 123 -> 321, -123 -> 321. If there is an overflow after reverse, return 0.
-	 * @param x
-	 * @return
-	 */
 	public int reverse(int x) {
 		boolean isPositive = true;
 		if (x < 0) {
 			isPositive = false;
 			x = -x;
 		}
-		//make sure x is positive
+		//保证x是正数
 		int res = 0;
 		while (x > 0) {
-			if (res > Integer.MAX_VALUE / 10) {
+			if (res > Integer.MAX_VALUE / 10) { //为什么不写成res * 10 > Integer.MAX_VALUE呢？
 				return 0;
 			}
 			int current = x % 10;
