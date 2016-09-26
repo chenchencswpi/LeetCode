ac8a698bf942096284056b1ebdd748b71fd22c87
chenchen <chenchencswpi@gmail.com>
2016-09-26 08:54:03 -0400

    403

--- LeetCode/src/$403/FrogJump.java
+++ LeetCode/src/$403/FrogJump.java
@@ -1,6 +1,8 @@
 package $403;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class FrogJump {
 	public boolean canCross1(int[] stones) {
@@ -28,5 +30,39 @@ public class FrogJump {
             }
         }
         return false;
+    }
+    
+    public boolean canCross2(int[] stones) {
+    	if (stones == null || stones.length < 2) {
+    		return true;
+    	}
+    	if (stones[1] != 1) {
+    		return false;
+    	}
+    	Queue<Entry> queue = new LinkedList<>();
+    	queue.offer(new Entry(1, 1));
+    	while (!queue.isEmpty()) {
+    		Entry currentPosition = queue.poll();
+    		if (currentPosition.index == stones.length - 1) {
+    			return true;
+    		}
+    		for (int k = currentPosition.lastJumpUnits - 1; k <= currentPosition.lastJumpUnits + 1; k++) {
+    			int nextIndex = Arrays.binarySearch(stones, currentPosition.index + 1, stones.length, 
+    					stones[currentPosition.index] + k);
+    			if (nextIndex > 0) {
+    				queue.offer(new Entry(nextIndex, k));
+    			}
+    		}
+    	}
+    	return false;
+    }
+    
+    class Entry {
+    	int index;
+    	int lastJumpUnits;
+    	public Entry(int index, int lastJumpUnits) {
+    		this.index = index;
+    		this.lastJumpUnits = lastJumpUnits;
+    	}
     }
 }
