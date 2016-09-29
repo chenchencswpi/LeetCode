4ff1d7fcc344a96d81da4b3d8ccd85720074e55f
chenchen <chenchencswpi@gmail.com>
2016-09-28 21:08:15 -0400

    111

--- LeetCode/src/$111/MinimumDepthOfBinaryTree.java
+++ LeetCode/src/$111/MinimumDepthOfBinaryTree.java
@@ -1,11 +1,14 @@
 package $111;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MinimumDepthOfBinaryTree {
-	public int minDepth(TreeNode root) {
+	public int minDepthDfs(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        int leftDepth = minDepth(root.left), rightDepth = minDepth(root.right);
+        int leftDepth = minDepthDfs(root.left), rightDepth = minDepthDfs(root.right);
         if (root.left != null && root.right != null) {
             return 1 + Math.min(leftDepth, rightDepth);
         } else if (root.left == null) {
@@ -14,6 +17,32 @@ public class MinimumDepthOfBinaryTree {
             return 1 + leftDepth;
         }
     }
+	
+	public int minDepthBfs(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		Queue<TreeNode> nodeQueue = new LinkedList<>();
+		Queue<Integer> depth = new LinkedList<>();
+		nodeQueue.offer(root);
+		depth.offer(1);
+		while (!nodeQueue.isEmpty()) {
+			TreeNode node = nodeQueue.poll();
+			int currentDepth = depth.poll();
+			if (node.left == null && node.right == null) {
+				return currentDepth;
+			}
+			if (node.left != null) {
+				nodeQueue.offer(node.left);
+				depth.offer(currentDepth + 1);
+			}
+			if (node.right != null) {
+				nodeQueue.offer(node.right);
+				depth.offer(currentDepth + 1);
+			}
+		}
+		return 0;
+	}
 }
 
 class TreeNode {
