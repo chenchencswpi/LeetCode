660341d7220e43ffb1877e86d0005e84fa32a615
chenchen <chenchencswpi@gmail.com>
2016-10-03 19:27:23 -0400

    133

--- LeetCode/src/$133/CloneGraph.java
+++ LeetCode/src/$133/CloneGraph.java
@@ -7,26 +7,48 @@ import java.util.List;
 import java.util.Queue;
 
 public class CloneGraph {
-	public static void main(String[] args) {
-		UndirectedGraphNode node = new UndirectedGraphNode(0);
-		node.neighbors.add(node);
-		node.neighbors.add(node);
-		new CloneGraph().cloneGraph(node);
-	}
-	//first we do bfs
-	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+	public UndirectedGraphNode cloneGraphBFS(UndirectedGraphNode node) {
         if (node == null) {
-        	return null;
+            return null;
         }
-        Queue<UndirectedGraphNode> queue = new LinkedList<>();
         HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
         UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
-        map.put(newNode, node);
-        for (UndirectedGraphNode neighbor: node.neighbors) {
-        	
-        	
+        map.put(node, newNode);
+        Queue<UndirectedGraphNode> queue = new LinkedList<>();
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode curr = queue.poll();
+            for (UndirectedGraphNode neighbor: curr.neighbors) {
+                if (!map.containsKey(neighbor)) {
+                    UndirectedGraphNode newNeighbor = new UndirectedGraphNode(neighbor.label);
+                    map.put(neighbor, newNeighbor);
+                    queue.offer(neighbor);
+                }
+                map.get(curr).neighbors.add(map.get(neighbor));
+            }
         }
-        return null;
+        return newNode;
+    }
+	
+	public UndirectedGraphNode cloneGraphDFS(UndirectedGraphNode node) {
+        if (node == null) {
+            return null;
+        }
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+        UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
+        map.put(node, newNode);
+        dfs(node, map);
+        return newNode;
+    }
+    
+    void dfs(UndirectedGraphNode node, HashMap<UndirectedGraphNode, UndirectedGraphNode> map) {
+        for (UndirectedGraphNode neighbor: node.neighbors) {
+            if (!map.containsKey(neighbor)) {
+                map.put(neighbor, new UndirectedGraphNode(neighbor.label));
+                dfs(neighbor, map);
+            }
+            map.get(node).neighbors.add(map.get(neighbor));
+        }
     }
 }
 
