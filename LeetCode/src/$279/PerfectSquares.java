c43abb2e5bc9b380980401d121e752e91b0fae40
chenchen <chenchencswpi@gmail.com>
2016-10-05 08:44:05 -0400

    279

---
+++ LeetCode/src/$279/PerfectSquares.java
@@ -0,0 +1,20 @@
+package $279;
+
+import java.util.Arrays;
+
+public class PerfectSquares {
+	public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (j * j == i) {
+                    dp[i] = 1;
+                } else if (j * j < i) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
