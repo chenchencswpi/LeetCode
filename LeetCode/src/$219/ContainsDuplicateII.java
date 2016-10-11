852c36c4883eb5569d5f90031ee5ab3198ae65a6
chenchen <chenchencswpi@gmail.com>
2016-10-11 17:21:31 -0400

    219

---
+++ LeetCode/src/$219/ContainsDuplicateII.java
@@ -0,0 +1,18 @@
+package $219;
+
+import java.util.HashMap;
+
+public class ContainsDuplicateII {
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
