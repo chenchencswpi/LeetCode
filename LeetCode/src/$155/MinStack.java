b647ede22908193c968fd301774bf6b4d89cbf63
chenchen <chenchencswpi@gmail.com>
2016-10-15 16:54:43 -0400

    155

---
+++ LeetCode/src/$155/MinStack.java
@@ -0,0 +1,34 @@
+package $155;
+
+import java.util.Stack;
+
+public class MinStack {
+	Stack<Integer> stack;
+    Stack<Integer> minStack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= getMin()) {
+            minStack.push(x);
+        }
+    }
+    
+    public void pop() {
+        if (stack.pop() == getMin()) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
